745f01dbf702c60056643772b53f3bbe26cec434
irik77587 <48862812+irik77587@users.noreply.github.com>
2020-08-09 14:16:58 +0600

    Update chat-server.js
    Declaring functions and variables before any execution

--- chat-server.js
+++ chat-server.js
@@ -1,17 +1,8 @@
+// Declaring functions and variables before any execution
 var sender = 'Nice1';
 var sent = 'col-md-8 col-sm-12 mb-3 pt-3 bg-primary text-white ml-auto rounded';
 var recieve = 'col-md-8 col-sm-12 mb-3 pt-3 bg-light border rounded';
-$(() => {
-  $.post('/conversation', (data) => {
-    $.each(data.conversation, (key,value) => {
-      var msgtyp = value.user == sender ? sent : recieve ;
-      var format = `<div class="${msgtyp}"><p>${value.user}</p><p>${value.text}</p></div>`;
-      $(format).appendTo('#conversation');
-    });
-    $(document).scrollTop($(document).height());
-  },'json');
-});
-var send_message = function()
+function send_message()
 {
   var message = $('#send-input').val();
   console.log(message);
@@ -24,9 +15,22 @@ var send_message = function()
   }
   return true;
 }
+
 $('#button-send').click(send_message);
+
 $('#send-input').keyup((event) => {
   if(event.which == 13)
     send_message();
   return false;
+});
+
+$(() => {
+  $.post('/conversation', (data) => {
+    $.each(data.conversation, (key,value) => {
+      var msgtyp = value.user == sender ? sent : recieve ;
+      var format = `<div class="${msgtyp}"><p>${value.user}</p><p>${value.text}</p></div>`;
+      $(format).appendTo('#conversation');
+    });
+    $(document).scrollTop($(document).height());
+  },'json');
 });
