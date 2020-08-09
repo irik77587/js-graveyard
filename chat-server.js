6e89207904e693d9d3e526fd9618a008553b85df
irik77587 <48862812+irik77587@users.noreply.github.com>
2020-08-09 13:47:19 +0600

    Create chat-server.js

---
+++ chat-server.js
@@ -0,0 +1,32 @@
+var sender = 'Nice1';
+var sent = 'col-md-8 col-sm-12 mb-3 pt-3 bg-primary text-white ml-auto rounded';
+var recieve = 'col-md-8 col-sm-12 mb-3 pt-3 bg-light border rounded';
+$(() => {
+  $.post('/conversation', (data) => {
+    $.each(data.conversation, (key,value) => {
+      var msgtyp = value.user == sender ? sent : recieve ;
+      var format = `<div class="${msgtyp}"><p>${value.user}</p><p>${value.text}</p></div>`;
+      $(format).appendTo('#conversation');
+    });
+    $(document).scrollTop($(document).height());
+  },'json');
+});
+var send_message = function()
+{
+  var message = $('#send-input').val();
+  console.log(message);
+  if (!(message === ''))
+  {
+    var format = `<div class="${sent}"><p>${sender}</p><p>${message}</p></div>`;
+    $(format).appendTo('#conversation');
+    $(document).scrollTop($(document).height());
+    $('#send-input').val('');
+  }
+  return true;
+}
+$('#button-send').click(send_message);
+$('#send-input').keyup((event) => {
+  if(event.which == 13)
+    send_message();
+  return false;
+});
