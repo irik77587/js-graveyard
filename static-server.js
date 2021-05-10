b3f6b4759185f1ebbabd44bbd891f9697d6be3a3
Ishrak <irik77587@gmail.com>
2021-05-10 06:05:57 +0600

    Update static-server.js
    decodeURIComponent for better integration with file system and simplify parent directory

--- static-server.js
+++ static-server.js
@@ -1,33 +1,30 @@
-var fs = require('fs'),
-    http = require('http');
-
-const arg = process.argv.slice(2),
+const fs = require('fs'),
+      http = require('http'),
+      arg = process.argv.slice(2),
       rootdir = arg[0] || process.cwd(),
-      port = arg[1] || 9000,
-      hostname = arg[2] || '127.0.0.1';
+      port = process.env.PORT || 9000,
+      hostname = process.env.HOST || '127.0.0.1';
 
 http.createServer(async function (req, res) {
 
   try{
-    var url = req.url;
-    var data;
-    var stats = await fs.statSync(rootdir + url);
+    req_url = decodeURIComponent(req.url);
+    stats = await fs.statSync(rootdir + req_url);
 
     if (stats.isFile()) {
-      data = await fs.readFileSync(rootdir + url);
+      data = await fs.readFileSync(rootdir + req_url);
       res.writeHead(200);
       res.end(data);
       return;
     }
 
     if (stats.isDirectory()) {
-      parent = url.split('/').slice(0,-1).join('/');
-      data = [`<a href="http://${hostname}:${port}${parent}">${url}</a>`];
-      dir = await fs.readdirSync(rootdir + url, {encoding:'utf8', withFileTypes:false});
-      dir.forEach(file=> data.push(`<a href="http://${hostname}:${port}${url}${url.slice(-1) == '/' ? '' : '/'}${file}">${file}</a>`));
+      parent = req_url.slice(0,req_url.lastIndexOf('/'));
+      data = [`<a href="http://${hostname}:${port}${parent}">..</a>`];
+      dir = await fs.readdirSync(rootdir + req_url, {encoding:'utf8', withFileTypes:false});
+      dir.forEach(file=> data.push(`<a href="http://${hostname}:${port}${req_url}${req_url.slice(-1) == '/' ? '' : '/'}${file}">${file}</a>`));
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data.join('<br>'));
-      data = [];
       return;
     }
 
