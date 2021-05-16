d432e003a49c4b9a520c32cbf5d98b7933f1902c
Ishrak <irik77587@gmail.com>
2021-05-16 20:59:02 +0600

    Extra comment added

--- upload.js
+++ upload.js
@@ -7,7 +7,7 @@ const fs = require('fs'),
 //tested on node=v10.19.0
 http.createServer(function(req, res) {
   if (req.method == "POST") {
-    // generate temporary file name
+    // Generate temporary file name
     var temp = 'temp' + Math.floor(Math.random() * 10);
 
     // This opens up the writeable stream to temporary file
@@ -21,15 +21,17 @@ http.createServer(function(req, res) {
     // This pipes the POST data to the file
     req.pipe(writeStream);
 
-    // After all the data is saved, respond with a simple html form so they can post more data
+    // After all the raw data is saved, close raw file
     req.on('end', () => {
       writeStream.close(() => {
 
+        // After closing raw file, create real file
         file = fs.readFileSync(temp);
         filename = file.slice(file.indexOf("filename=\"") + "filename=\"".length, file.indexOf('"\r\nContent-Type'));
         hash = file.slice(0,file.indexOf('\r\n'));
         content = file.slice(file.indexOf('\r\n\r\n') + '\r\n\r\n'.length, file.lastIndexOf(Buffer.from('\r\n') + hash));
 
+        // After real file is created, delete raw file
         fs.writeFile(filename.toString(), content, err => {
           if (err)
             console.error(err);
@@ -40,12 +42,12 @@ http.createServer(function(req, res) {
     });
   }
 
+  // respond with a simple html form so they can post more data
   res.writeHead(200, {"content-type":"text/html; charset=utf-8"});
   res.end(`
 <form  method="post" enctype="multipart/form-data">
   <input type="file" name="fileUpload">
   <input type="submit" value="Upload">
 </form>`);
-
 }).listen(port, host, () => console.dir(`Serving at http://${host}:${port}`));
 
