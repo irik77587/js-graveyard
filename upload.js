d489b31bf1f5e9eb1c29d179d026e8a627200b2f
Ishrak <irik77587@gmail.com>
2021-05-17 15:38:15 +0600

    all file operations inside try catch block

--- upload.js
+++ upload.js
@@ -6,35 +6,36 @@ const fs = require('fs'),
       host = process.env.HOST || '127.0.0.1';
 //tested on node=v10.19.0, export HOST="192.168.0.103"
 http.createServer(function(req, res) {
-  if (req.method == "POST") {
+  if (req.method == "POST") try {
     // Generate temporary file name
     var temp = 'temp' + Math.floor(Math.random() * 10);
 
     // This opens up the writeable stream to temporary file
     var writeStream = fs.createWriteStream(temp);
 
-    // This is here incase any errors occur
-    writeStream.on('error', err => console.log(err));
-
     // This pipes the POST data to the file
     req.pipe(writeStream);
 
-    // After all the temporary file is creates, create real file
+    // After the temporary file is creates, create real file
     writeStream.on('finish', () => {
 
-      file = fs.readFileSync(temp);
-      filename = file.slice(file.indexOf("filename=\"") + "filename=\"".length, file.indexOf('"\r\nContent-Type'));
-      hash = file.slice(0,file.indexOf('\r\n'));
-      content = file.slice(file.indexOf('\r\n\r\n') + '\r\n\r\n'.length, file.lastIndexOf(Buffer.from('\r\n') + hash));
+      reader = fs.readFileSync(temp);
+      filename = reader.slice(reader.indexOf("filename=\"") + "filename=\"".length, reader.indexOf('"\r\nContent-Type'));
+      hash = reader.slice(0,reader.indexOf('\r\n'));
+      content = reader.slice(reader.indexOf('\r\n\r\n') + '\r\n\r\n'.length, reader.lastIndexOf(Buffer.from('\r\n') + hash));
 
       // After real file is created, delete temporary file
-      fs.writeFile(filename.toString(), content, err => {
-        if (err)
-          console.error(err);
-        else
-          try { fs.unlinkSync(temp) } catch (err) { console.error(err) }
-      });
+      fs.writeFileSync(filename.toString(), content);
+      fs.unlinkSync(temp);
+
+//      fs.writeFile(filename.toString(), content, err => fs.unlinkSync(temp));
+
     });
+
+    // This is here incase any errors occur
+  } catch (err) {
+    res.end('Server Borked');
+    return;
   }
 
   // respond with a simple html form so they can post more data
