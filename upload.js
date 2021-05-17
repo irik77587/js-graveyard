28a14a02f3b3750dce82d5ce77e100e2e4107aa6
Ishrak <irik77587@gmail.com>
2021-05-17 09:27:45 +0600

    Callback hell reduce
    Listen to writestream finish emitter instead of passing writestream.close to req.end

--- upload.js
+++ upload.js
@@ -3,8 +3,8 @@ const fs = require('fs'),
       args = process.argv.slice(2),
       rootdir = args[0] || process.cwd(),
       port = process.env.PORT || 9000,
-      host = process.env.HOST || '127.0.0.1';;
-//tested on node=v10.19.0
+      host = process.env.HOST || '127.0.0.1';
+//tested on node=v10.19.0, export HOST="192.168.0.103"
 http.createServer(function(req, res) {
   if (req.method == "POST") {
     // Generate temporary file name
@@ -14,30 +14,25 @@ http.createServer(function(req, res) {
     var writeStream = fs.createWriteStream(temp);
 
     // This is here incase any errors occur
-    writeStream.on('error', function (err) {
-      console.log(err);
-    });
+    writeStream.on('error', err => console.log(err));
 
     // This pipes the POST data to the file
     req.pipe(writeStream);
 
-    // After all the raw data is saved, close raw file
-    req.on('end', () => {
-      writeStream.close(() => {
+    // After all the temporary file is creates, create real file
+    writeStream.on('finish', () => {
 
-        // After closing raw file, create real file
-        file = fs.readFileSync(temp);
-        filename = file.slice(file.indexOf("filename=\"") + "filename=\"".length, file.indexOf('"\r\nContent-Type'));
-        hash = file.slice(0,file.indexOf('\r\n'));
-        content = file.slice(file.indexOf('\r\n\r\n') + '\r\n\r\n'.length, file.lastIndexOf(Buffer.from('\r\n') + hash));
+      file = fs.readFileSync(temp);
+      filename = file.slice(file.indexOf("filename=\"") + "filename=\"".length, file.indexOf('"\r\nContent-Type'));
+      hash = file.slice(0,file.indexOf('\r\n'));
+      content = file.slice(file.indexOf('\r\n\r\n') + '\r\n\r\n'.length, file.lastIndexOf(Buffer.from('\r\n') + hash));
 
-        // After real file is created, delete raw file
-        fs.writeFile(filename.toString(), content, err => {
-          if (err)
-            console.error(err);
-          else
-            try { fs.unlinkSync(temp) } catch (err) { console.error(err) }
-        });
+      // After real file is created, delete temporary file
+      fs.writeFile(filename.toString(), content, err => {
+        if (err)
+          console.error(err);
+        else
+          try { fs.unlinkSync(temp) } catch (err) { console.error(err) }
       });
     });
   }
