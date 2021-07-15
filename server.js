10b297bf016f8bffd0c0f67407f64d9eda8cce2d
Ishrak <irik77587@gmail.com>
2021-07-16 00:34:45 +0600

    http 206 partial content video
    workaround for video not playing in iPhone

--- server.js
+++ server.js
@@ -3,43 +3,63 @@ const fs = require('fs'),
       arg = process.argv.slice(2),
       rootdir = arg[0] || process.cwd(),
       port = process.env.PORT || 9000,
-      hostname = process.env.HOST || '127.0.0.1';
+      host = process.env.HOST || '127.0.0.1';
 //tested on node=v10.19.0
 http.createServer(function (req, res) {
 
+	// Normalize request.url into path and get status of path
   try {
     req_url = decodeURIComponent(req.url).replace(/\/+/g, '/');
     stats = fs.statSync(rootdir + req_url);
+    console.dir(req_url + ' ' + rootdir);
 
+		// If path is file, response content of file
     if (stats.isFile()) {
-      buffer = fs.createReadStream(rootdir + req_url);
+    	[start, end] = [0, stats.size]
+    	range = req.headers.range;
+    	if (range) {// workaround for safari
+    		[start_range, end_range] = range.replace("bytes=","").split("-");
+    		[start, end] = [parseInt(start_range, 10), parseInt(end_range, 10)]
+    		if(end + 1 == stats.size) {
+    			end++;
+    		}
+    		res.writeHead(206, { 
+    			'Content-Range': `bytes ${start}-${end}/${stats.size}`, 
+    			'Accept-Ranges': 'bytes', 
+    			'Content-Length': end-start
+    		});
+    	}
+    	console.dir([start, end, stats.size]);
+    	// actually send content of file
+      buffer = fs.createReadStream(rootdir + req_url, { start: start, end: end });
       buffer.on('open', () => buffer.pipe(res));
     }
 
+		// If path is folder, response html page listing content in folder
     if (stats.isDirectory()) {
       lsof = fs.readdirSync(rootdir + req_url, {encoding:'utf8', withFileTypes:false});
       res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-      res.end(html_page(`http://${hostname}:${port}`, req_url, lsof));
+      res.end(html_page(`http://${host}:${port}`, req_url, lsof));
     }
 
   } catch (err) {
-      res.writeHead(404);
-      res.end("Not found");
+    	res.writeHead(404, {"content-type":"text/plain; charset=utf-8"});// header; 404 goes here
+    	res.end("Server Borked");// body
       console.dir(err);
   }
-}).listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`));
+}).listen(port, host, () => console.dir(`Server running at http://${host}:${port}`));
 
 
-function html_page(host, req_url, lsof) {//this is a Function declarations can be called before it is defined
+function html_page(root_url, url, ls_dir) {//this is a Function declarations can be called before it is defined
 
-  list = req_url == '/' ? [] : [`<div style = "grid-column: 1 / span 3;"><a href="${host}">/</a></div>`,
-  `<div style = "grid-column: 1 / span 3;"><a href="${host}${encodeURI(req_url.slice(0,req_url.lastIndexOf('/')))}">..</a></div>`];
+  list = url == '/' ? [] : [`<div style = "grid-column: 1 / span 3;"><a href="${root_url}">/</a></div>`,
+  `<div style = "grid-column: 1 / span 3;"><a href="${root_url}${encodeURI(url.slice(0,url.lastIndexOf('/')))}">..</a></div>`];
 
-  templete = (host, req_url, file) => {// the above is a Function expressions cannot be called before it is defined
-    return `<div><a href="${host}${encodeURI(req_url)}${req_url.slice(-1) == '/' ? '' : '/'}${encodeURI(file)}">${file}</a></div>`; }
+  templete = (root_url, url, filename) => {// the above is a Function expressions cannot be called before it is defined
+    return `<div><a href="${root_url}${encodeURI(url)}${url.slice(-1) == '/' ? '' : '/'}${encodeURI(filename)}">${filename}</a></div>`; }
 
-  lsof.forEach(file => {
-    list.push(templete(host, req_url, file));
+  ls_dir.forEach(file => {
+    list.push(templete(root_url, url, file));
   });
 
   return `
@@ -48,10 +68,10 @@ function html_page(host, req_url, lsof) {//this is a Function declarations can b
 <head>
   <meta http-equiv="content-type" content="text/html" charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Directory of ${req_url}</title>
+  <title>Directory of ${url}</title>
 </head>
 <body>
-<h1>Directory of ${req_url}</h1>
+<h1>Directory of ${url}</h1>
 <style>
 .grid-container{
   display: grid;
@@ -62,6 +82,6 @@ function html_page(host, req_url, lsof) {//this is a Function declarations can b
 ${list.join('\n')}
 </div>
 </body>
-</html>`
+</html>`;
 }
 
